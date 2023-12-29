6106e4210932497e491caf00a01686cf7c303dd2
Amoriss <amoriss@protonmail.com>
2023-12-29 00:19:21 -0600

    Add project files.

---
+++ Debugging_Demo/Methods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debugging_Demo;
+internal class Methods
+{
+    private static Random r = new Random(); 
+    
+    public static void DoesSomething()
+    {
+        //int randomNumber = r.Next(1, 10);
+        Console.Write("Type something here: ");
+        var parsedNumber = int.Parse(Console.ReadLine());   
+        Console.WriteLine($"random {r.Next(1, 10)}");
+        Console.WriteLine();
+    }
+}
---
+++ Debugging_Demo/Program.cs
@@ -0,0 +1,33 @@
+﻿using Debugging_Demo;
+
+// Logical Error
+Console.WriteLine("World, Hello!");
+
+//Runtime Error
+//if you don't enter an int, there will be a runtime error
+var answer = int.Parse(Console.ReadLine());
+
+//tryparse prevents a runtime error
+var couldParse = int.TryParse(Console.ReadLine(), out int number);
+
+number = 10;
+
+//locals window will show you all the variables within the scope you're in
+//call stack will show which method i'm currently in
+
+Methods.DoesSomething();
+int x;
+int y;
+int z;
+
+//Runtime - Divide by Zero Exception
+Console.WriteLine("Enter in a number that I can divide by");
+
+int divideBy= int.Parse(Console.ReadLine());
+
+Console.WriteLine(number/divideBy);
+
+//Runtime - Out of range exception
+
+int[] array = new int[] { 1, 2, 3 };
+Console.WriteLine(array[3]);
